d789cced940a010fd2ec2fe11709a1501779eca0
Paul Clay <livemixlove@users.noreply.github.com>
2019-02-18 23:46:02 -0800

    comment out tests

--- src/App.js
+++ src/App.js
@@ -13,14 +13,15 @@ class App extends Component {
     }
 
     startAppWithResponseFromOuttracker(){
-        MessageRelayer.processMessage('@outtracker help', demoUserName)
+        MessageRelayer.processMessage('@outtracker', demoUserName)
         // uncomment these lines if you want to prepopulate chat
-        MessageRelayer.processMessage('@outtracker start', demoUserName)
-        MessageRelayer.processMessage('@outtracker describe "some description of outage"', demoUserName)
-        MessageRelayer.processMessage('@outtracker record', demoUserName)
-        MessageRelayer.processMessage('hello hello', demoUserName)
-        MessageRelayer.processMessage('great words in a message', demoUserName)
-        MessageRelayer.processMessage('@outtracker end', demoUserName)
+        // MessageRelayer.processMessage('@outtracker help', demoUserName)
+        // MessageRelayer.processMessage('@outtracker start', demoUserName)
+        // MessageRelayer.processMessage('@outtracker describe "some description of outage"', demoUserName)
+        // MessageRelayer.processMessage('@outtracker record', demoUserName)
+        // MessageRelayer.processMessage('hello hello', demoUserName)
+        // MessageRelayer.processMessage('great words in a message', demoUserName)
+        // MessageRelayer.processMessage('@outtracker end', demoUserName)
     }
 
     render() {
